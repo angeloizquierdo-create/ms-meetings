1d7d8d8de4ccca46686d8319ec2c5bceba646a29
angeloizquierdo-create <angelo.izquierdo@uai.edu.pe>
2025-10-03 09:27:08 -0500

    Update cors.js

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,14 +4,16 @@ dotenv.config();
 
 const allowedOrigins = [
     process.env.ALLOWED_ORIGIN_N8N,
-    process.env.ALLOWED_ORIGIN_FIREBASE_ZOOM_MANAGER_PREVIEW,
     process.env.ALLOWED_ORIGIN_FIREBASE_ZOOM_MANAGER_PRODUCTION,
+    process.env.ALLOWED_ORIGIN_FIREBASE_ZOOM_MANAGER_PRODUCTION_ALT, // <-- AÑADIDO
+    process.env.ALLOWED_ORIGIN_FIREBASE_ZOOM_MANAGER_PREVIEW,
 ];
 
 const corsOptions = {
     origin: process.env.CORS_ALLOW_ALL === 'true'
         ? '*'
         : function (origin, callback) {
+            // Permitir peticiones sin origen (como las de Postman o apps móviles)
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
