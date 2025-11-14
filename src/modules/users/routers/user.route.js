29f18f5ffa42c974a8d1204bc03da80fe7781a4c
angeloizquierdo-create <angelo.izquierdo@uai.edu.pe>
2025-11-14 14:33:56 -0500

    Delete src directory

--- src/modules/users/routers/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { 
-    getUserByEmail 
-} from '../controllers/user.controller.js';
-
-const UserRouter = Router();
-UserRouter.get('/by-email/:email', getUserByEmail);
-
-export default UserRouter;
